f0d7dfe0d340a4d38091131a2233c2fc42a50672
zhenchao <zhenchao.wang@hotmail.com>
2018-02-06 23:18:26 +0800

    add flf files

--- src/main/java/org/zhenchao/App.java
+++ src/main/java/org/zhenchao/App.java
@@ -1,13 +1,19 @@
 package org.zhenchao;
 
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.util.Collection;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) throws Exception {
+        Collection<File> files = FileUtils.listFiles(new File("src/main/resources"), new String[] {"txt"}, false);
+        for (final File file : files) {
+            String newName = file.getName().replace(".txt", ".flf");
+            file.renameTo(new File(newName));
+        }
     }
 }
